b3cca5477f9ab3c8abb3858fc7bf7254a7c3aa62
Ing-Andy <lefilsdecathy@gmail.com>
2025-04-08 17:21:57 +0200

    oui je recommence

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+export default function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
